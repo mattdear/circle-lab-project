695f988d559dbb21876c62d0a8046778ec8f3f15
Mark Hartop <markhartop@Marks-MacBook-Pro.local>
2020-03-30 08:26:13 +0100

    index page added

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include("OOP/WebPage.php");
+$webPage = new WebPage("Login", "Circle Lab", 2020);
+$webPage->open();
+$webPage->setCSS("css/smart-system");
+$webPage->writeHead();
+?>
+    <div id="login">
+        <h1>Log In</h1>
+        <form method="post" action="php/login.php">
+            <label for="username">Username:</label><br/>
+            <input name="username" id="username"/> <br/>
+            <label for="password">Password:</label><br/>
+            <input name="password" id="password" type="password"/> <br/>
+            <input class="button" type="submit" value="Sign In!"/>
+        </form>
+    </div>
+<?php
+$webPage->writeFooter();
+$webPage->close();
