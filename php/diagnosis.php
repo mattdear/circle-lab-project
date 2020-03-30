5b358ff9d4ac5f20151dd54372a54f7dbec6fa32
Mark Hartop <markhartop@Marks-MacBook-Pro.local>
2020-03-30 11:26:30 +0100

    diagnosis page added with updates to css and hompage

--- php/diagnosis.php
+++ php/diagnosis.php
@@ -5,4 +5,20 @@ $webPage->open();
 $webPage->setCSS("../css/smart-system");
 $webPage->writeHead();
 ?>
-<h1>Hello World</h1>
+    <h1>Diagnosis</h1>
+    <form>
+        <label>
+            <input name="symptom" placeholder="Search Symptoms">
+        </label>
+    </form>
+    <div id="symptomResults">
+
+    </div>
+
+    <button onclick="window.location.href = 'addDiagnosis.php';">Add Diagnosis</button>
+    <br/>
+    <button onclick="window.location.href = 'homepage.php';">Homepage</button>
+    <br/>
+<?php
+$webPage->writeFooter();
+$webPage->close();
--- php/homepage.php
+++ php/homepage.php
@@ -5,15 +5,21 @@ $webPage->open();
 $webPage->setCSS("../css/smart-system");
 $webPage->writeHead();
 ?>
-<div>
-    <h1>Homepage</h1>
-    <button onclick="window.location.href = 'diagnosis.php';">Diagnosis</button><br/>
-    <button onclick="window.location.href = 'people.php';">People</button><br/>
-    <button onclick="window.location.href = 'appointments.php';">Appointments</button><br/>
-    <button onclick="window.location.href = 'locations.php';">Locations</button><br/>
-    <button onclick="window.location.href = 'prescriptions.php';">Prescriptions</button><br/>
-    <button onclick="window.location.href = 'reports.php';">Reports</button><br/>
-</div>
+    <div>
+        <h1>Homepage</h1>
+        <button onclick="window.location.href = 'diagnosis.php';">Diagnosis</button>
+        <br/>
+        <button onclick="window.location.href = 'people.php';">People</button>
+        <br/>
+        <button onclick="window.location.href = 'appointments.php';">Appointments</button>
+        <br/>
+        <button onclick="window.location.href = 'locations.php';">Locations</button>
+        <br/>
+        <button onclick="window.location.href = 'prescriptions.php';">Prescriptions</button>
+        <br/>
+        <button onclick="window.location.href = 'reports.php';">Reports</button>
+        <br/>
+    </div>
 <?php
 $webPage->writeFooter();
 $webPage->close();
