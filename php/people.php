bc12c5dbf4f72de81fadb352e7ee1f1b604a70ab
Mark Hartop <markhartop@Marks-MacBook-Pro.local>
2020-03-30 11:35:59 +0100

    people page added, additional css modifications

--- php/people.php
+++ php/people.php
@@ -1,21 +1,21 @@
 <?php
 include("../OOP/WebPage.php");
-$webPage = new WebPage("Diagnosis", "Circle Lab", 2020);
+$webPage = new WebPage("People", "Circle Lab", 2020);
 $webPage->open();
 $webPage->setCSS("../css/smart-system");
 $webPage->writeHead();
 ?>
-    <h1>Diagnosis</h1>
+    <h1>People</h1>
     <form>
         <label>
-            <input class="inputs" name="symptom" placeholder="Search Symptoms">
+            <input  class="inputs" name="people" placeholder="Search People">
         </label>
     </form>
     <div id="symptomResults">
 
     </div>
 
-    <button onclick="window.location.href = 'addDiagnosis.php';">Add Diagnosis</button>
+    <button onclick="window.location.href = 'addPerson.php';">Add Diagnosis</button>
     <br/>
     <button onclick="window.location.href = 'homepage.php';">Homepage</button>
     <br/>
