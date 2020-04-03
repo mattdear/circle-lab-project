ab5b9413a15e0d98c6e2c5abe15963a3fae2cf93
Matt Dear <mrmdear@gmail.com>
2020-04-03 20:10:43 +0100

    I have now written the prescription DTO.

---
+++ prescriptionDTO.php
@@ -0,0 +1,85 @@
+<?php
+
+class prescriptionDTO
+{
+
+    private $id, $patient, $date, $quantity, $location, $isactive;
+
+    public function __construct($id, $patient, $date, $quantity, $location, $isactive)
+    {
+        $this->id = $id;
+        $this->patient = $patient;
+        $this->date = $date;
+        $this->quantity = $quantity;
+        $this->location = $location;
+        $this->isactive = $isactive;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getPatient()
+    {
+        return $this->patient;
+    }
+
+    public function setPatient($patient)
+    {
+        $this->patient = $patient;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    public function getIsactive()
+    {
+        return $this->isactive;
+    }
+
+    public function setIsactive($isactive)
+    {
+        $this->isactive = $isactive;
+    }
+
+    public function toString()
+    {
+        echo $this->id . " " . $this->patient . " " . $this->date . " " . $this->quantity . " " . $this->location . " " . $this->isactive . "<br />";
+    }
+
+}
+
+?>
