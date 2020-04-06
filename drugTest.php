edec3fcaf58dfd76990fa1242e1d418fe9bd6dae
Joshua Alsop-Barrell <joshnash1998@gmail.com>
2020-04-06 11:46:32 +0100

    Added DTO

---
+++ drugTest.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class drug
+{
+    //Attributes
+    private $id;
+    private $name;
+
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function toString()
+    {
+        $string = "Name: $this->name";
+    }
+
+}
