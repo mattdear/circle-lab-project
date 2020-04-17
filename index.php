c720bc498f9c3f85dcb4e783c8494ed205972bc8
Mark Hartop <markhartop@Marks-MacBook-Pro.local>
2020-04-17 17:31:22 +0100

    added redirect code if already logged in

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ if (!isset ($_SESSION["gatekeeper"])) {
     <div id="login">
         <h1>Log In</h1>
         <form method="post" action="php/login.php">
-            <label for="username">Username:</label><br/>
-            <input name="username" id="username"/> <br/>
-            <label for="password">Password:</label><br/>
-            <input name="password" id="password" type="password"/> <br/>
-            <input class="button" type="submit" value="Sign In!"/>
+            <label for="username">Username:</label><br/><br/>
+            <input name="username" class="inputs" id="username"/><br/><br/>
+            <label for="password">Password:</label><br/><br/>
+            <input name="password" class="inputs" id="password" type="password"/> <br/><br/>
+            <button type="submit">Sign In!</button>
         </form>
     </div>
     <?php
