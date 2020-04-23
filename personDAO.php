af7b9e8c0fb4097a3339dec5def5588708b28e08
emma12812 <emma12812@gmail.com>
2020-04-23 08:47:19 +0100

    updates

--- personDAO.php
+++ personDAO.php
@@ -1,12 +1,15 @@
 <?php
 include(personDTO.php");
+
 class personDAO
 {
     private $table, $conn;
+
     public function __construct($conn , $table){
         $this->conn = $conn;
         $this->table = $table;
     }
+
     //Inserting a new person
     public function insertPerson(personDTO $newPerson){
       if($newPerson != null && $newPerson->getId() == null && $newPerson->getFirstName() != null && $newPerson->getLastName() && $newPerson->getDob() != null && $newPerson->getGender() != null && $newPerson->getEmail() != null && $newPerson->getPhone() != null && $newPerson->getAddress() != null && $newPerson->getRole() != null && $newPerson->getUsername() != null && $newPerson->getPassword() != null {
@@ -16,12 +19,15 @@ class personDAO
         $newPerson->setId($id);
       }
         return $newPerson;
+
     }
+
     //Updating a person
     public function updatePerson(personDTO $updatedPerson){
         $stmt = $this->conn->prepare("UPDATE " . $this->table .  " SET first_name= ? , last_name= ? , dob= ? , gender= ? , email= ? , phone= ? , address= ? , role= ? , username= ? , password= ?  WHERE id= ?");
         $stmt->execute([$updatedPerson->getFirstName(), $updatedPerson->getLastName(), $updatedPerson->getDob() , $updatedPerson->getGender(), $updatedPerson->getEmail() , $updatedPerson->getPhone(), $updatedPerson->getAddress() , $updatedPerson->getRole(), $updatedPerson->getUsername(), $updatedPerson->getPassword(), $updatedPerson->getId()]);
     }
+
     //Find and return all people
     public function findAllPeople(){
         $stmt = $this->conn->query("SELECT * FROM ".  $this->table);
@@ -31,6 +37,7 @@ class personDAO
         }
         return $people;
     }
+
     //Find person by template
     public function findMatchingPeople(personDTO $personTemplate){
       if($personTemplate != null){
@@ -94,8 +101,10 @@ class personDAO
                   }
                   return $people;
                 }
+
                 return null;
     }
+
     //Find by role
     public function findByRole($role){
         if($role != null){
@@ -109,6 +118,7 @@ class personDAO
       }
       return null;
     }
+
     //Find by Id
     public function findPersonById($id){
       if($id != null){
