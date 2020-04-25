7793bfddabb06444a00c478ba6e4049dc81c5ff3
jcdms21622125 <49288496+jcdms21622125@users.noreply.github.com>
2020-04-25 16:06:11 +0100

    Update AppointmentDao.php
    addded if statement and null checks with return null in case of null

--- AppointmentDao.php
+++ AppointmentDao.php
@@ -14,12 +14,14 @@ class AppointmentDao
 //add a new Appointment//
     public function AddAppointment($newAppointment)
     {
-       if($newAppointment != null && $newAppointment->getId() == null && $newAppointment->getName() != null)
+       if($newAppointment != null && $newAppointment->getId() == null && $newAppointment->getdescription() != null &&
+          $newAppointment->getpatient() != null && $newAppointment->getstaff_member() != null && $newAppointment->getdate_time() != null &&
+          $newAppointment->getlocation() != null && $newAppointment->getIsactive() != null)
             {
         $stmt = $this->conn->prepare("INSERT INTO " . $this->table . "( description, patient, staff_member, date_time, location,
-            duration, isactive) VALUES (?,?,?,?,?,?,?)");
+                duration, isactive) VALUES (?,?,?,?,?,?,?)");
         $stmt->execute([$newAppointment->getdescription(), $newAppointment->getpatient(), $newAppointment->getstaff_member(),
-            $newAppointment->getdate_time(), $newAppointment->getlocation(), $newAppointment->getIsactive()]);
+                $newAppointment->getdate_time(), $newAppointment->getlocation(), $newAppointment->getIsactive()]);
         $id = (int)$this->conn->lastInsertId();
         $newAppointment->setId($id);
            return $newAppointment;
@@ -29,15 +31,22 @@ class AppointmentDao
 
     //find all Appointment List//
     public function FindAllAppointment()
-    {
+     {
+       if($newAppointment->getId() != null && $newAppointment->getdescription() != null && $newAppointment->getpatient() != null
+          && $newAppointment->getstaff_member() != null && $newAppointment->getdate_time() != null &&
+          $newAppointment->getlocation() != null && $newAppointment->getIsactive() != null)
+            {
         $stmt = $this->conn->prepare("SELECT * FROM " . $this->table);
         $Appointment = [];
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+            {
             array_push($people, new AppointmentDto($row["id"], $row["description"], $row["patient"],
                 $row["patient"], $row["staff_member"], $row["date_time"], $row["location"], $row["duration"], $row["isactive"]));
-        }
+            }
         return $Appointment;
-        }
+            }
+      return false
+      }
     public function FindAllAppointmentByPatient(AppointmentDto $findAllAppointmentByPatient)
     {
         $stmt = $this->conn->prepare("SELECT * FROM " . $this->table.  " WHERE patient= id");
